b2833b5bb35b495b1078f9a9674a4c814dd37fcf
aryan <50gamer.aamg@gmail.com>
2021-08-28 17:34:30 +0430

    projects

---
+++ projects/week8/s1/script.js
@@ -0,0 +1,11 @@
+var x = parseInt(prompt('type first'));
+var y = parseInt(prompt('type second'));
+
+var t = typeof x ;
+var y = typeof y ;
+
+if (t == y) {
+  console.log('the same');
+}else if (t !== y) {
+
+}
